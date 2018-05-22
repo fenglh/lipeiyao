f58d82040bf8bc4599a75da336210eecb778e7d0
itx <>
2018-05-22 09:21:11 +0800

    增加注册页面

---
+++ RFIDAPP/RFIDAPP/Application/Controllers/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  RFIDAPP
+//
+//  Created by fenglh on 2018/5/22.
+//  Copyright © 2018年 Apple Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
